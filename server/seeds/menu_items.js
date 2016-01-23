598bc4e914f05634812626e023c0789eb60466b7
adrianbadarau <adi.badarau@yahoo.com>
2016-01-23 11:17:53 +0200

    modified menu items seeds

---
+++ server/seeds/menu_items.js
@@ -0,0 +1,28 @@
+/**
+ * Created by adrianbadarau on 23/01/16.
+ */
+Meteor.startup(function(){
+    console.log(MenuItems.find().count());
+    if (MenuItems.find().count() === 0){
+        MenuItems.insert({
+            label:'DashBoard',
+            route:'admin',
+            icon: 'fa fa-tachometer'
+        });
+        MenuItems.insert({
+            label:'Pages',
+            route:'admin.pages',
+            icon: 'fa fa-file',
+            action_button:{
+                route:'admin.pages.create',
+                icon:'fa fa-plus-square',
+                label:'Add Page'
+            }
+        });
+        MenuItems.insert({
+            label:'Posts',
+            route:'admin.posts',
+            icon: 'fa fa-file-text'
+        });
+    }
+});
